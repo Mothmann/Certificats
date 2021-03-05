157bf89f0bef3e3970cf63ad18fbf3116b0e6403
Mothmann <othman.lahlou00@gmail.com>
2021-03-05 23:42:58 +0100

    added home route to admin only buttons left now

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +17,26 @@ class MainController extends AbstractController
     {
         $id = $this->getUser();
         $name = $this->getDoctrine()->getRepository(User::class)->name($id);
+        $admin = $this->getDoctrine()->getRepository(User::class)->find($id);
+        $check = $admin->getRoles();
+        if ($check == ["ROLE_ADMIN","ROLE_USER"]){
+           return($this->redirectToRoute('admin_home'));
+        }
+        else {
+            return $this->render('main/index.html.twig', [
+            'names' => $name,
+        ]);
+        }
+
+    }
+    /**
+     * @Route("/admin", name="admin_home")
+     */
+    public function admin(): Response{
+        $id = $this->getUser();
+        $name = $this->getDoctrine()->getRepository(User::class)->name($id);
+        $admin = $this->getDoctrine()->getRepository(User::class)->find($id);
+        $check = $admin->getRoles();
         return $this->render('main/index.html.twig', [
             'names' => $name,
         ]);
